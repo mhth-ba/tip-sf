98c0fc5c267467b015d7d5e5ff896052e239be0d
Juraj Martak <martak.juraj@gmail.com>
2019-11-05 17:08:44 +0100

    Skutocna cena tepla - revizia:
     - upload repository

--- src/AppBundle/Repository/Kontroling/SCT/UploadRepository.php
+++ src/AppBundle/Repository/Kontroling/SCT/UploadRepository.php
@@ -41,18 +41,6 @@ class UploadRepository extends EntityRepository
             ->getOneOrNullResult();
     }
 
-    public function getLastUploadedDanoveOdpisy($id)
-    {
-        return $this->createQueryBuilder('u')
-            ->setMaxResults(1)
-            ->andWhere('u.upload = 4')
-            ->andWhere('u.hlavny = :id')
-            ->setParameter('id', $id)
-            ->orderBy('u.datum', 'desc')
-            ->getQuery()
-            ->getOneOrNullResult();
-    }
-
     public function uploadDodaneTeplo($id)
     {
         return $this->createNativeNamedQuery('Excel_DodaneTeplo')
@@ -63,13 +51,6 @@ class UploadRepository extends EntityRepository
     public function uploadSkutocneNaklady($id)
     {
         return $this->createNativeNamedQuery('Excel_SkutocneNaklady')
-            ->setParameter('id', $id)
-            ->execute();
-    }
-
-    public function uploadDanoveOdpisy($id)
-    {
-        return $this->createNativeNamedQuery('Excel_DanoveOdpisy')
             ->setParameter('id', $id)
             ->execute();
     }
