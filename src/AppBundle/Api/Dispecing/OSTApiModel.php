e5732f0ed55d65f456261eb4501d1d62208e0a40
DorienMay <dorien@dorienmay.com>
2025-04-09 19:22:14 +0200

    _RefOST api model

---
+++ src/AppBundle/Api/Dispecing/OSTApiModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Api\Dispecing;
+
+use AppBundle\Api\DefaultApiModel;
+
+class OSTApiModel extends DefaultApiModel
+{
+    public $id;
+
+    public $cislo;
+
+    public $adresa;
+
+    public $typ;
+}
