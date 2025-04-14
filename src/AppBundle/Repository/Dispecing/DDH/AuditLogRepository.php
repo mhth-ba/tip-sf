0d22d03177dbde5c0a989ba5305646cbbd252c41
DorienMay <dorien@dorienmay.com>
2025-04-14 12:47:01 +0200

    limited serialization of "pouzivatel" object
    response was too big, causing errors like:
    - Fatal Error: Maximum execution time of 300 seconds exceeded
    - OutOfMemoryException - exhausted

--- src/AppBundle/Repository/Dispecing/DDH/AuditLogRepository.php
+++ src/AppBundle/Repository/Dispecing/DDH/AuditLogRepository.php
@@ -8,20 +8,24 @@ class AuditLogRepository extends EntityRepository
 {
     public function findUserActivityAll()
     {
-        return $this->createQueryBuilder('a')
+        $results = $this->createQueryBuilder('a')
             ->orderBy('a.id', 'desc')
             ->getQuery()
-            ->execute();
+            ->getResult();
+
+        return $this->formatResults($results);
     }
 
     public function findUserActivityByHlavny($id)
     {
-        return $this->createQueryBuilder('a')
+        $results = $this->createQueryBuilder('a')
             ->where('a.hlavny = :id')
             ->setParameter('id', $id)
             ->orderBy('a.id', 'desc')
             ->getQuery()
-            ->execute();
+            ->getResult();
+
+        return $this->formatResults($results);
     }
 
     public function findUserActivityByDate($date)
@@ -32,13 +36,57 @@ class AuditLogRepository extends EntityRepository
         $endDate = clone $date;
         $endDate->setTime(23, 59, 59);
 
-        return $this->createQueryBuilder('a')
+        $results = $this->createQueryBuilder('a')
             ->where('a.vytvorene >= :startDate')
             ->andWhere('a.vytvorene <= :endDate')
             ->setParameter('startDate', $startDate)
             ->setParameter('endDate', $endDate)
             ->orderBy('a.id', 'desc')
             ->getQuery()
-            ->execute();
+            ->getResult();
+
+        return $this->formatResults($results);
+    }
+
+    /**
+     * Format the results to match the desired output structure
+     *
+     * @param array $results
+     * @return array
+     */
+    private function formatResults(array $results)
+    {
+        $formattedResults = [];
+
+        foreach ($results as $result) {
+            $formattedResult = [
+                'id' => $result->getId(),
+                'hlavny' => $result->getHlavny(),
+                'hodnota' => $result->getHodnota(),
+                'riadok' => $result->getRiadok(),
+                'stlpec' => $result->getStlpec(),
+                'tabulka' => $result->getTabulka(),
+                'vytvorene' => $result->getVytvorene() instanceof \DateTime ?
+                    $result->getVytvorene()->getTimestamp() : $result->getVytvorene(),
+            ];
+
+            if ($result->getPouzivatel()) {
+                $user = $result->getPouzivatel();
+                $formattedResult['pouzivatel'] = [
+//                    'id' => $user->getId(),
+//                    'username' => $user->getUsername(),
+                    'fullname' => $user->getFullname(),
+//                    'mail' => $user->getMail(),
+//                    'title' => $user->getTitle(),
+//                    'department' => $user->getDepartment()
+                ];
+            } else {
+                $formattedResult['pouzivatel'] = null;
+            }
+
+            $formattedResults[] = $formattedResult;
+        }
+
+        return $formattedResults;
     }
 }
