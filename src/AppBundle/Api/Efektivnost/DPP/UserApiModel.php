b93582bccee332a02367ddd35c881e90c5aa64dc
DorienMay <dorien@dorienmay.com>
2025-04-14 05:48:20 +0200

    add

---
+++ src/AppBundle/Api/Efektivnost/DPP/UserApiModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Api\Efektivnost\DPP;
+
+use AppBundle\Api\DefaultApiModel;
+
+class UserApiModel extends DefaultApiModel
+{
+    public $id;
+
+    public $fullname;
+
+    public $mail;
+
+    public $title;
+}
